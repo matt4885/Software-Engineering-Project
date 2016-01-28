dcbb9b12b32a501d2ab23302b664077f44c9bf4f
Matthew Kempey <mkempey@gmail.com>
2016-01-28 15:45:03 -0500

    Added src/ folder and main.java

---
+++ src/main.java
@@ -0,0 +1,18 @@
+/* Project_Name
+ * Team 5 (Knee Deep In The Thread)
+ * v0.001
+ * Date of Last Modification: January 28th, 2016 @ 03:41P.M.
+ * Authors:
+ * Matthew Kempey
+ * Andy Nguyen
+ * Gary Adams
+ * Justy Jan Tubalado
+ * Kevin Jones
+ */
+
+
+public class Project {
+	public static void main(String[] args) {
+		System.out.print("Hello, world!");
+	}
+}
